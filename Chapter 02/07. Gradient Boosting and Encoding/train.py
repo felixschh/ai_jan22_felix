d179b47da147ed1ac5c0d12251d2c1c4425818be
Felix Schekerka <felixschekerka@gmail.com>
2022-03-11 11:11:11 +0100

    adding svm

--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -1,5 +1,21 @@
 import data_handler as dh
 
-x_train, x_test, y_train, y_test = dh.get_data("./insurance.csv")
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
-print(dh.hello)
+x_train, x_test, y_train, y_test = dh.get_data("insurance.csv")
+
+def train_models(x_train, y_train):
+    
+    dt = DecisionTreeRegressor(max_depth=5, random_state=42)
+    dt = dt.fit(x_train, y_train)
+
+    rf = RandomForestRegressor(n_estimators=50, random_state=42)
+    rf = rf.fit(x_train, y_train)
+
+    gbt = GradientBoostingRegressor(random_state=42)
+    gbt = gbt.fit(x_train, y_train)
+
+    return dt, rf, gbt
+
