bd508f8fefe53a5f0b438fbc1a30670fdaee04ee
Felix Schekerka <felixschekerka@gmail.com>
2022-04-06 17:48:38 +0200

    first tries

--- Chapter 03/03. MLP Regression/model.py
+++ Chapter 03/03. MLP Regression/model.py
@@ -1,11 +1,28 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torchsummary import summary
+# from torchsummary import summary
 
 # YOUR CODE HERE
+class Network(nn.Module):
+    
+    # Defining the layers
+    def __init__(self):
+        super(Network, self).__init__()
+        self.fc1 = nn.Linear(8, 16)
+        self.fc2 = nn.Linear(16, 8)
+        self.fc3 = nn.Linear(8, 1)
+        
+    # Forward pass through
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
 
-
-
+model = Network()
+model
 
         
--- Chapter 03/03. MLP Regression/train.py
+++ Chapter 03/03. MLP Regression/train.py
@@ -8,8 +8,55 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
-from model import #import your model here
+from model import Network
 
+x_train, x_test, y_train, y_test = dh.load_data('Chapter 03/03. MLP Regression/data/turkish_stocks.csv')
+
+model = Network()
+
+lr = 1e-1
+n_epochs = 100
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.2)
+loss_func = torch.nn.MSELoss()
+
+for epoch in range(n_epochs):
+    model.train()
+    optimizer.zero_grad()
+
+    y_hat = model(x_train)
+    
+    loss = loss_func(y_train, y_hat)
+    loss.backward()    
+    optimizer.step()
+    losses = loss.item()
+
+    
+train_loader = dh.to_batches(x_train, x_test, y_train, y_test, 20)
+
+losses = []
+val_losses = []
+
+for epoch in range(n_epochs):
+    for x_batch, y_batch in train_loader:
+        x_batch = x_batch
+        y_batch = y_batch
+
+        loss = losses(x_batch, y_batch)
+        losses.append(loss)
+        
+    with torch.no_grad():
+        for x_val, y_val in val_loader:
+            x_val = x_val
+            y_val = y_val
+            
+            model.eval()
+
+            yhat = model(x_val)
+            val_loss = loss_func(y_val, yhat)
+            val_losses.append(val_loss.item())
+
+print(model.state_dict())
 
 
 # Remember to validate your model: with torch.no_grad() ...... model.eval .........model.train
